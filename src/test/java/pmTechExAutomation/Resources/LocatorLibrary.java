2972daaa43e11cfa3f6ae7970a7a894537f7db39
Grizel Olmilla <>
2023-09-04 11:06:08 +0800

    Initialized test files

---
+++ src/test/java/pmTechExAutomation/Resources/LocatorLibrary.java
@@ -0,0 +1,5 @@
+package pmTechExAutomation.Resources;
+
+public class LocatorLibrary {
+    
+}
---
+++ src/test/java/pmTechExAutomation/TestCases/AccountActivationRegression.java
@@ -0,0 +1,16 @@
+package pmTechExAutomation.TestCases;
+
+import org.testng.annotations.Test;
+
+import pmTechExAutomation.Resources.LocatorLibrary;
+
+
+public class AccountActivationRegression {
+
+    @Test
+    public void sampleTest() {
+        // nothing
+    }
+    
+}
+
